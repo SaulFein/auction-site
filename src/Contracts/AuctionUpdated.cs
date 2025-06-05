bf28ba23ecef6f87ed224f416cfb1b09001f22b4
Brandon Feinstein <brandon.feinstein@hotmail.com>
2025-06-04 22:17:54 -0700

    publish auction event rabbitmq

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,20 @@
+namespace Contracts;
+
+public class AuctionUpdated
+{
+    public string Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Movement { get; set; }
+    public string CaseMaterial { get; set; }
+    public string Bracelet { get; set; }
+    public string DialColor { get; set; }
+    public string Condition { get; set; }
+    public bool Box { get; set; }
+    public bool Papers { get; set; }
+    public string CaseSize { get; set; }
+    public string CaseThickness { get; set; }
+    public string WaterResistance { get; set; }
+    public string Description { get; set; }
+}
